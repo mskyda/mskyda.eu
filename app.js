48a635641456f1366ad63d1ae06ebeac5ac13782
Mischa Skyda <mykhailo.skyda@share-now.com>
2020-10-20 15:58:58 +0200

    add subdomains

--- app.js
+++ app.js
@@ -1,34 +1,29 @@
-// Copyright 2017 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 'use strict';
 
 // [START gae_node_request_example]
 const express = require('express');
+const vhost = require('vhost');
+const path = require('path');
 
 const app = express();
+const mischa = express();
+const ira = express();
+
+app.use(vhost('mischa.skyda.eu', mischa));
+app.use(vhost('ira.skyda.eu', ira));
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello, world!').end();
+  res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-// Start the server
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
-  console.log('Press Ctrl+C to quit.');
+mischa.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/mischa.html'));
 });
-// [END gae_node_request_example]
+
+ira.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/ira.html'));
+});
+
+app.listen(process.env.PORT || 8080);
 
 module.exports = app;
