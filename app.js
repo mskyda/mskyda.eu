cb45450a65ffa29a7679862fc5343830898dea6b
Mischa Skyda <mykhailo.skyda@share-now.com>
2021-02-25 08:18:20 +0100

    add Finik

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const path = require('path');
 const app = express();
 const mischa = express();
 const ira = express();
+const finik = express();
 
 app.use(vhost('mischa.skyda.eu', mischa));
 app.use(vhost('ira.skyda.eu', ira));
+app.use(vhost('finik.skyda.eu', finik));
 
 app.use(express.static(__dirname + '/public/root/'));
 mischa.use(express.static(__dirname + '/public/mischa/'));
 ira.use(express.static(__dirname + '/public/ira/'));
+finik.use(express.static(__dirname + '/public/finik/'));
 
 
 app.listen(process.env.PORT || 8080);
