a252998abf634a2f164ad67a706d64e2561852b8
Mischa Skyda <mykhailo.skyda@share-now.com>
2021-05-07 10:07:09 +0200

    add healthchecks

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const vhost = require('vhost');
-const path = require('path');
+const https = require('https');
 
 const app = express();
 const mischa = express();
@@ -18,7 +18,10 @@ mischa.use(express.static(__dirname + '/public/mischa/'));
 ira.use(express.static(__dirname + '/public/ira/'));
 finik.use(express.static(__dirname + '/public/finik/'));
 
-
 app.listen(process.env.PORT || 8080);
+
+const pingURL = 'https://hc-ping.com/bbe983a2-ecb2-4113-a662-db781a84612e'; // todo: move to env vars
+
+setInterval(() => { https.get(pingURL); }, 5 * 60000);
 
 module.exports = app;
