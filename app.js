f3c1b4a7d87024594521b332443a03dcfec797d5
Mischa Skyda <mykhailo.skyda@share-now.com>
2021-07-18 16:42:01 +0200

    finish telegram bot

--- app.js
+++ app.js
@@ -2,9 +2,7 @@
 require('dotenv').config();
 const express = require('express');
 const vhost = require('vhost');
-const http = require('http');
 const https = require('https');
-const TelegramBot = require('node-telegram-bot-api');
 
 const app = express();
 const mischa = express();
@@ -22,65 +20,8 @@ finik.use(express.static(__dirname + '/public/finik/'));
 
 app.listen(process.env.PORT || 8080);
 
-const pingURL = 'https://hc-ping.com/bbe983a2-ecb2-4113-a662-db781a84612e'; // todo: move to env vars
-
-setInterval(() => { https.get(pingURL); }, 5 * 60000);
+setInterval(() => { https.get(process.env.PING_URL); }, 5 * 60000);
 
 module.exports = app;
 
-const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
-
-let status;
-let chatID;
-
-bot.on('message', (msg) => {
-
-  chatID = chatID || msg.chat.id;
-
-  if(msg.text.length === 6 && Number.isInteger(+msg.text)){
-
-    checkStatus(msg.text, true);
-
-    setTimeout(() => { checkStatus(msg.text); }, 10000);
-
-  } else {
-
-    bot.sendMessage(chatID, 'Enter Pass ID - must be 6 digits')
-
-  }
-
-});
-
-function checkStatus(passID, toShowResult){
-
-  http.request({
-    host: 'passport.mfa.gov.ua',
-    path: `/Home/CurrentSessionStatus?sessionId=${passID}`
-  }, (response) => {
-
-    let str = '';
-
-    response.on('data', (chunk) => {str += chunk;});
-
-    response.on('end', () => {
-
-      if(!str){
-        bot.sendMessage(chatID, 'Wrong ID');
-        return;
-      }
-
-      if(status && status !== str){
-
-        bot.sendMessage(chatID, 'STATUS CHANGED');
-
-      }
-
-      status = str;
-
-      if(toShowResult){ bot.sendMessage(chatID, status); }
-
-    });
-
-  }).end();
-
-}
+require('./telegram-bot');
---
+++ telegram-bot.js
@@ -0,0 +1,96 @@
+const http = require('http');
+const TelegramBot = require('node-telegram-bot-api');
+
+const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {polling: true});
+
+let status;
+let chatID;
+let timeout;
+
+bot.on('message', (msg) => {
+
+  chatID = chatID || msg.chat.id;
+
+  if(msg.text.length === 6 && Number.isInteger(+msg.text)){
+
+    checkStatus(msg.text, true);
+
+  } else if (msg.text.toLowerCase() === 'stop') {
+
+    if(timeout){ clearTimeout(timeout); }
+
+    bot.sendMessage(chatID, 'Бот остановлен');
+
+  }else {
+
+    bot.sendMessage(chatID, 'Введи номер заказа паспорта (6 цифр)');
+
+  }
+
+});
+
+function checkStatus(passID, showResult){
+
+  http.request({
+    host: 'passport.mfa.gov.ua',
+    path: `/Home/CurrentSessionStatus?sessionId=${passID}`
+  }, (response) => {
+
+    let str = '';
+
+    response.on('data', (chunk) => {str += chunk;});
+
+    response.on('end', () => {
+
+      if(!str){
+        bot.sendMessage(chatID, `Нет данных о заказе #${passID}`);
+        return;
+      }
+
+      if(showResult || (status && status !== str)){
+
+        bot.sendMessage(chatID, parseResponse(str));
+
+        bot.sendMessage(chatID, `Когда статус заказа #${passID} изменится - бот сообщит об этом \nВведи STOP чтобы не получать обновления`);
+
+      } else {
+
+        console.log(`${parseDate(new Date)} (${passID}) - status unchanged`);
+
+      }
+
+      timeout = setTimeout(() => { checkStatus(passID); }, 60 * 1000);
+
+      status = str;
+
+    });
+
+  }).end();
+
+}
+
+function parseResponse(resp){
+
+  resp = JSON.parse(resp);
+
+  let parsed = `Статус заказа #${resp.UserSessionId}:`;
+
+  resp.StatusInfo.forEach((step) => {
+
+    parsed += `\n ${parseDate(new Date(step.StatusDateUF))} - ${step.StatusName}`;
+
+  });
+
+  return parsed;
+
+}
+
+function parseDate(date){
+
+  let minutes = date.getMinutes();
+
+  minutes = (minutes < 10 ? '0' : '') + minutes;
+
+  return `${date.getHours()}:${minutes} ${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+
+}
