90e7f9e828ab79b3f0a7e7a38ed439871c0d725e
Mischa <mischa@MacBook-Pro-Ira.local>
2021-11-01 10:40:52 +0100

    add IP detection. finalise MVP

--- resolve-dns.js
+++ resolve-dns.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const publicIP = require('public-ip');
 
 const requestConfig = {
     headers: {
@@ -7,17 +8,34 @@ const requestConfig = {
     }
 };
 
-(async () => {
+let currentIP;
 
-    const dnsRecords = await getDNSRecords();
-    const recordsIP = dnsRecords[0].content;
-    const currentIP = "8.8.8.8";
+setInterval(checkIP,  60000);
 
-    if(recordsIP !== currentIP){
-        await updateDNSRecord(dnsRecords[0], currentIP);
+checkIP();
+
+async function checkIP() {
+
+    const newIP = await publicIP.v4();
+
+    if(!newIP){ return; }
+
+    if(!currentIP){ currentIP = newIP; }
+
+    if(currentIP !== newIP){
+        currentIP = newIP;
+        updateDNSRecords();
     }
 
-})();
+}
+
+async function updateDNSRecords() {
+
+    const dnsRecords = await getDNSRecords();
+
+    for (const record of dnsRecords) { await updateDNSRecord(record); }
+
+}
 
 async function getDNSRecords() {
 
@@ -27,10 +45,10 @@ async function getDNSRecords() {
     return data && data.result;
 }
 
-async function updateDNSRecord({id, type, name, proxied}, newIP) {
+async function updateDNSRecord({id, type, name, proxied}) {
 
     const updateEndpoint = `https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/dns_records/${id}`;
-    const updateData = {type, name, proxied, content: newIP};
+    const updateData = {type, name, proxied, content: currentIP};
 
     await axios.put(updateEndpoint, updateData, requestConfig);
 
