d03598ecee7d34716f323f0f0500df2c8464c368
Mischa <mischa@MacBook-Pro-Ira.local>
2021-11-01 11:05:52 +0100

    add logging. make checkIP call sequestial

--- resolve-dns.js
+++ resolve-dns.js
@@ -8,32 +8,38 @@ const requestConfig = {
     }
 };
 
-let currentIP;
-
-setInterval(checkIP,  60000);
-
 checkIP();
 
-async function checkIP() {
+async function checkIP(currentIP) {
 
     const newIP = await publicIP.v4();
 
     if(!newIP){ return; }
 
-    if(!currentIP){ currentIP = newIP; }
+    if(!currentIP){
+        console.info(`[DNS]: first run. set current IP to ${newIP}`);
+        currentIP = newIP;
+    }
 
     if(currentIP !== newIP){
+        console.info(`[DNS]: IP has changed from ${currentIP} to ${newIP}`);
+        await updateDNSRecords(newIP);
         currentIP = newIP;
-        updateDNSRecords();
     }
+
+    setTimeout(() => { checkIP(currentIP); },  5 * 60000);
 
 }
 
-async function updateDNSRecords() {
+async function updateDNSRecords(newIP) {
 
     const dnsRecords = await getDNSRecords();
+    console.info(`[DNS]: fetched ${dnsRecords.length} records`);
 
-    for (const record of dnsRecords) { await updateDNSRecord(record); }
+    for (const record of dnsRecords) {
+        console.info(`[DNS]: update record "${record.name}"`);
+        await updateDNSRecord(record, newIP);
+    }
 
 }
 
@@ -45,10 +51,10 @@ async function getDNSRecords() {
     return data && data.result;
 }
 
-async function updateDNSRecord({id, type, name, proxied}) {
+async function updateDNSRecord({id, type, name, proxied}, newIP) {
 
     const updateEndpoint = `https://api.cloudflare.com/client/v4/zones/${process.env.CLOUDFLARE_ZONE_ID}/dns_records/${id}`;
-    const updateData = {type, name, proxied, content: currentIP};
+    const updateData = {type, name, proxied, content: newIP};
 
     await axios.put(updateEndpoint, updateData, requestConfig);
 
